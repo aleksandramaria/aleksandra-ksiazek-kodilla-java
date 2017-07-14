c3a6a3651fe820336b18db0964f861add72591dd
aleksandramaria <ola.ksi@gmail.com>
2017-07-14 16:34:44 +0100

    first commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,10 +1,25 @@
 package com.kodilla.testing;
 
-/**
- * Created by ola on 12/07/2017.
- */
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
-    public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+
+    public static void main(String[] args) throws java.lang.Exception {
+        Calculator calculator = new Calculator();
+        int resultOfAdding = calculator.addAToB(8, 2);
+        int resultOfSubtracting = calculator.subtractAFromB(8, 2);
+
+        if (resultOfAdding == 10) {
+            System.out.println("adding methos works");
+        } else {
+            System.out.println("boo :(");
+        }
+
+        if (resultOfSubtracting == 6) {
+            System.out.println("subtracting method works");
+        } else {
+            System.out.println("boo :(");
+        }
+
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,14 @@
+package com.kodilla.testing.calculator;
+
+/**
+ * Created by ola on 14/07/2017.
+ */
+public class Calculator {
+    public int addAToB(int a, int b) {
+        return a + b;
+    }
+
+    public int subtractAFromB(int a, int b) {
+        return a - b;
+    }
+}
