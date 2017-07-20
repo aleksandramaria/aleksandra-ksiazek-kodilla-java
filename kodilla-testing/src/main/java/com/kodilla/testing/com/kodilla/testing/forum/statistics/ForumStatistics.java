80819e85e0c03e7f522ffa8576c7df11b25a242f
aleksandramaria <ola.ksi@gmail.com>
2017-07-20 13:54:10 +0100

    first commit

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/com/kodilla/testing/forum/statistics/ForumStatistics.java
@@ -0,0 +1,46 @@
+package com.kodilla.com.kodilla.testing.forum.statistics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by ola on 20/07/2017.
+ */
+public class ForumStatistics {
+    Statistics statistics;
+    private List<String> usersNames = new ArrayList<String>();
+    private int postsCount;
+    private int commentsCount;
+
+    public ForumStatistics(Statistics statistics, int postsCount, int commentsCount) {
+        this.statistics = statistics;
+        this.postsCount = postsCount;
+        this.commentsCount = commentsCount;
+    }
+
+    public int getPostsCount() {
+        return postsCount;
+    }
+
+    public int getCommentsCount() {
+        return commentsCount;
+    }
+
+    public getStatistics() {
+        return statistics;
+    }
+    int avePostsPerUser(){
+        return this.postsCount / usersNames.size();
+    }
+
+    int aveCommentsPerUser(){
+        return this.commentsCount / usersNames.size();
+    }
+    int aveCommentsPerPost(){
+        return this.commentsCount / this.postsCount;
+    }
+
+    public int calculateAdvStatistics(Statistics statistics) {
+        return ...;
+    }
+}
