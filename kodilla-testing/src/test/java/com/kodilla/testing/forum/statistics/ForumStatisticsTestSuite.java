92ea88d78604e0707969e1dec59c41e15e19e1b8
aleksandramaria <ola.ksi@gmail.com>
2017-07-22 13:24:22 +0100

    first commit

--- kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/ForumStatisticsTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/ForumStatisticsTestSuite.java
@@ -12,31 +12,33 @@ import static org.mockito.Mockito.when;
  * Created by ola on 21/07/2017.
  */
 public class ForumStatisticsTestSuite {
-    @Test
+   @Test
     public void testAvePostsPerUser() {
         //Given
         Statistics statisticsMock = mock(Statistics.class);
         List<String> users = new List<>();
         Random theGenerator = new Random();
-        for (int n=0; n<1000; n++){
+        for (int n = 0; n < 1000; n++) {
             int numberOfStr = theGenerator.nextInt(10);
 
             StringBuilder word = new StringBuilder();
 
-            for(int j = 0; j < numberOfStr; j++){
+            for (int j = 0; j < numberOfStr; j++) {
                 word.append("bla");
             }
 
             users.add(word.toString());
 
-        when(statisticsMock.usersNames().size()).thenReturn(users.size());
+            when(statisticsMock.usersNames().size()).thenReturn(users.size());
 
-        ForumStatistics forumStatistics = new ForumStatistics(statisticsMock);
+            ForumStatistics forumStatistics = new ForumStatistics(statisticsMock);
 
-        //When
-        int quantityOfUsers = forumStatistics.usersNames().size();
+            //When
+        /* int quantityOfUsers = forumStatistics.usersNames().size();
 
         //Then
-        Assert.assertEquals(1000, quantityOfUsers);
+        Assert.assertEquals(1000, quantityOfUsers);*/
+        }
     }
 }
+
