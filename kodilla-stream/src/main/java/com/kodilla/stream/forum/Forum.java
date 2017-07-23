d7489132d0f1330df283268a24cfaa927875f898
aleksandramaria <ola.ksi@gmail.com>
2017-07-23 19:25:43 +0100

    first commit

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/forum/Forum.java
@@ -0,0 +1,7 @@
+package com.kodilla.stream.forum;
+
+/**
+ * Created by ola on 23/07/2017.
+ */
+public class Forum {
+}
---
+++ kodilla-stream/src/main/java/com/kodilla/stream/person/People.java
@@ -0,0 +1,7 @@
+package com.kodilla.stream.person;
+
+/**
+ * Created by ola on 23/07/2017.
+ */
+public class People {
+}
