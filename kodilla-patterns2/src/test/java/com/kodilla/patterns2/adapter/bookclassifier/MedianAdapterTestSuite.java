b9317f67e5623ba3188f4eb66a8ed7a475aa14a3
aleksandramaria <ola.ksi@gmail.com>
2017-10-31 12:57:16 +0000

    adapter

--- kodilla-patterns2/src/test/java/com/kodilla/patterns2/adapter/bookclassifier/MedianAdapterTestSuite.java
+++ kodilla-patterns2/src/test/java/com/kodilla/patterns2/adapter/bookclassifier/MedianAdapterTestSuite.java
@@ -16,11 +16,14 @@ public class MedianAdapterTestSuite {
     public void publicationYearMedianTest() {
         //Given
         Set<Book> books = new HashSet<>();
+        books.add(new Book("A twist in the tale", "Jeffrey Archer", 1988, "ARC"));
+        books.add(new Book("The Book Thief", "Markus Zusak", 2005, "ZUS"));
+        books.add(new Book("Animal farm", "George Orwell", 1945, "ORW"));
         MedianAdapter medianAdapter = new MedianAdapter();
         //When
         int median = medianAdapter.publicationYearMedian(books);
         //Then
         System.out.println(median);
-        assertEquals(median,0, 0);
+        assertEquals(median, 1988, 0);
     }
 }
