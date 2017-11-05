8f1b6e2280c496e7815d931b4f7ee3c79c97132c
aleksandramaria <ola.ksi@gmail.com>
2017-11-05 20:30:56 +0000

    new facade

--- kodilla-patterns2/src/test/java/com/kodilla/patterns2/facade/employees/EmployeeFacadeTestSuite.java
+++ kodilla-patterns2/src/test/java/com/kodilla/patterns2/facade/employees/EmployeeFacadeTestSuite.java
@@ -15,14 +15,18 @@ public class EmployeeFacadeTestSuite {
 
     @Test
     public void testCompanyName() throws NameFindProcessingException {
+        //Given
         Company softwareMachine = new Company("Software Machine");
         Company dataMaesters = new Company("Data Maesters");
         Company greyMatter = new Company("Grey Matter");
+
+        //When
         try {
             employeeFacade.getForName("achi");
         } catch (NameFindProcessingException e) {
             //handling a problem
         }
+
         //Then
         List<Company> achiCompanies = employeeFacade.getForName("achi");
         Assert.assertEquals(1, achiCompanies.size());
@@ -30,14 +34,18 @@ public class EmployeeFacadeTestSuite {
 
     @Test
     public void testEmployeeName() throws NameFindProcessingException {
+        //Given
         Employee johnSmith = new Employee("John", "Smith");
         Employee stephanieClarckson = new Employee("Stephanie", "Clarckson");
         Employee lindaKovalsky = new Employee("Linda", "Kovalsky");
+
+        //When
         try {
             employeeFacade.getForFirstName("lin");
         } catch (NameFindProcessingException e) {
             //
         }
+
         //Then
         List<Employee> linEmployees = employeeFacade.getForFirstName("lin");
         Assert.assertEquals(1, linEmployees.size());
